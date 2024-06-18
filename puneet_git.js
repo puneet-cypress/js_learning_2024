83b332f465fa584c68e78ba9754df0f39f309a76
Puneet Aneja <puneetaneja@Puneets-MacBook-Air.local>
2024-06-18 12:34:16 -0700

    Psecondcommit

--- puneet_git.js
+++ puneet_git.js
@@ -1,4 +1,11 @@
-let sum = (a,b);
-sum = a+b
+// let sum = (a,b);
+// sum = a+b
 
-console.log(sum(2,3)); // 5
+// console.log(sum(2,3)); // 5
+
+function  sum(a,b){
+
+   console.log(a+b)
+}
+
+sum(2,3); // 5
